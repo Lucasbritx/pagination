644501a4fc61502e3ab9a3a7faf9ca2acc1bff94
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2026-06-14 00:11:24 -0300

    feat: offset pagination example

--- src/pages/offset-pagination/index.tsx
+++ src/pages/offset-pagination/index.tsx
@@ -1,7 +1,47 @@
-export default function OffsetPagination() {
+import { useQuery } from "@tanstack/react-query";
+import { useSearchParams } from "react-router";
+
+const pageSize = 20;
+
+export function OffsetPaginationPage() {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const page = Number(searchParams.get("page") ?? 1);
+  const offset = (page - 1) * pageSize;
+
+  const { data, isLoading } = useQuery({
+    queryKey: ["pokemon-offset", page],
+    queryFn: async () => {
+      const res = await fetch(
+        `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${pageSize}`,
+      );
+      return res.json();
+    },
+  });
+
+  if (isLoading) return <p>Loading...</p>;
+
+  function goToPage(nextPage: number) {
+    setSearchParams({ page: String(nextPage) });
+  }
+
   return (
-    <section>
-      <h1>Offset pagination example</h1>
-    </section>
+    <div>
+      <h1>Offset Pagination</h1>
+
+      {data.results.map((pokemon: any) => (
+        <p key={pokemon.name}>{pokemon.name}</p>
+      ))}
+
+      <button disabled={page === 1} onClick={() => goToPage(page - 1)}>
+        Previous
+      </button>
+
+      <span> Page {page} </span>
+
+      <button disabled={!data.next} onClick={() => goToPage(page + 1)}>
+        Next
+      </button>
+    </div>
   );
 }
