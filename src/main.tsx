2df37d08ccb31db3e054bb7502ac8839692f9df1
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2026-06-19 00:23:05 -0300

    feat: add routes for client and cursor pagination pages

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createBrowserRouter, Navigate, RouterProvider } from "react-router";
 import "./index.css";
 import { OffsetPaginationPage } from "./pages/offset-pagination";
+import { ClientPaginationPage } from "./pages/client-pagination";
+import { CursorPaginationPage } from "./pages/cursor-pagination";
 
 const queryClient = new QueryClient();
 
@@ -14,6 +16,14 @@ const router = createBrowserRouter([
   {
     path: "/offset-pagination",
     element: <OffsetPaginationPage />,
+  },
+  {
+    path: "/client-pagination",
+    element: <ClientPaginationPage />,
+  },
+  {
+    path: "/cursor-pagination",
+    element: <CursorPaginationPage />,
   },
 ]);
 
